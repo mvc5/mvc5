302ce9f3e62fbd8c9c2cd41a0456bdaaab11eee8
devosc <devosc@gmail.com>
2015-12-20 16:45:41 -0600

    event model

--- src/Event.php
+++ src/Event.php
@@ -11,13 +11,48 @@ class Event
     /**
      *
      */
-    use Event\Signal;
+    use Event\Model;
+    use Signal;
+
+    /**
+     * @var mixed
+     */
+    protected $model;
 
     /**
      * @param string|null $event
+     * @param mixed $model
      */
-    public function __construct($event = null)
+    public function __construct($event = null, $model = null)
     {
         $this->event = $event;
+        $this->model = $model;
+    }
+
+    /**
+     * @return array
+     */
+    protected function args()
+    {
+        return array_filter([
+            Arg::EVENT => $this,
+            Arg::MODEL => $this->model
+        ]);
+    }
+
+    /**
+     * @param callable $callable
+     * @param array $args
+     * @param callable $callback
+     * @return mixed
+     */
+    public function __invoke(callable $callable, array $args = [], callable $callback = null)
+    {
+        $model = $this->signal($callable, $this->args() + $args, $callback);
+
+        null !== $model
+            && $this->model = $model;
+
+        return $model;
     }
 }
