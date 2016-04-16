6cdd3fb106b2ad12dcabd914083912019d19b320
devosc <devosc@gmail.com>
2016-04-16 09:38:27 -0500

    simplify and fix scope

--- src/Plugin/Provider.php
+++ src/Plugin/Provider.php
@@ -5,34 +5,37 @@
 
 namespace Mvc5\Plugin;
 
-use Mvc5\Service\Scope;
+use Mvc5\Config\Configuration;
+use Mvc5\Plugins as _Plugins;
+use Mvc5\Resolvable;
 use Mvc5\Service\Service as _Service;
 
 class Provider
     extends Call
 {
     /**
-     * Constructor lists the class name, Plugins|Scope object, and additional constructor args
-     * @param $config
-     * @param array ...$args
+     * Creates an object with a Plugins container and sets the object as the scope of the anonymous functions. If the
+     * object is cloned, it will need a clone method to workaround the recursion problem.
+     *
+     * @param string $name
+     * @param array|Configuration|Resolvable $config
      */
-    public function __construct($config, ...$args)
+    public function __construct($name, $config = [])
     {
-        parent::__construct([$this, 'provider'], [new Link, $config, new Args($args)]);
+        parent::__construct([$this, 'provider'], [new Link, new Plugin(_Plugins::class, [$config, new Link]), $name]);
     }
 
     /**
      * @param _Service $service
-     * @param $config
-     * @param array $args
+     * @param _Plugins $plugins
+     * @param string $name
      * @return callable|null|object
      */
-    public function provider(_Service $service, $config, array $args = [])
+    public function provider(_Service $service, _Plugins $plugins, $name)
     {
-        $plugin = $service->plugin($config, $args);
+        $plugin = $service->plugin($name, [$plugins]);
 
-        $args && $args[0] instanceof Scope &&
-            $args[0]->scope(true);
+        $plugins->scope($plugin);
 
         return $plugin;
     }
