0a4ebcb6f61e326b644bfc77293290c2b6c088a1
devosc <devosc@gmail.com>
2016-05-25 22:50:50 -0500

    add middleware config

---
+++ config/middleware.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ */
+
+return [
+    'web' => [
+        'web\route',
+        'web\error',
+        'web\service',
+        'web\dispatch',
+        'web\status',
+        'web\version',
+        'web\send',
+    ],
+    'web\response' => [
+        'web\controller',
+        'web\layout',
+        'web\render',
+    ],
+];
