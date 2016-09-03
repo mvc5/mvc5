79c43a9442939c14120c5c5494cc922d5e332780
devosc <devosc@gmail.com>
2016-09-03 01:29:23 -0500

    change output buffer level var name

--- src/View/Template/Render.php
+++ src/View/Template/Render.php
@@ -52,7 +52,7 @@ trait Render
 
                 extract($this->vars());
 
-                $__ob_get_level = ob_get_level();
+                $__output_buffer_level__ = ob_get_level();
 
                 ob_start();
 
@@ -64,7 +64,7 @@ trait Render
 
                 } catch (Throwable $exception) {
 
-                    while(ob_get_level() > $__ob_get_level) {
+                    while(ob_get_level() > $__output_buffer_level__) {
                         ob_end_clean();
                     }
 
