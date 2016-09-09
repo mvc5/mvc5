8570b801449eeca7776f29ae615aab1dece03ab0
devosc <devosc@gmail.com>
2016-09-08 21:58:03 -0500

    session model

---
+++ src/Session/Model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Session;
+
+use Mvc5\Config\Configuration;
+use Mvc5\Config\Overload;
+
+class Model
+    implements Configuration
+{
+    /**
+     *
+     */
+    use Overload;
+}
