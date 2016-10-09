63748df7a3ddb564895614bb9063de749410dbd8
devosc <devosc@gmail.com>
2016-10-08 19:58:02 -0500

    add support for route collections by directly traversing child routes

---
+++ src/Route/Dispatch/Collection.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Route\Dispatch;
+
+use Mvc5\Arg;
+use Mvc5\Http\Request;
+use Mvc5\Request\Request as _Request;
+
+class Collection
+{
+    /**
+     *
+     */
+    use Router;
+
+    /**
+     * @param $name
+     * @param $parent
+     * @return string
+     */
+    protected function name($name, $parent)
+    {
+        return !$parent ? $name : $parent . Arg::SEPARATOR . $name;
+    }
+
+    /**
+     * @param Request $request
+     * @return _Request
+     */
+    function __invoke(Request $request)
+    {
+        return $this->result($request, $this->traverse($this->routeRequest($request), $this->route));
+    }
+}
