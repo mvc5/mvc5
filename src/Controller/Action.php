e16aeeaa1eb512bd695767f4161d9f934de64c5e
devosc <devosc@gmail.com>
2016-10-23 01:43:50 -0500

    allow controller to not exist, automatically enable service controllers

--- src/Controller/Action.php
+++ src/Controller/Action.php
@@ -6,6 +6,8 @@
 namespace Mvc5\Controller;
 
 use Mvc5\Plugin;
+use Mvc5\Resolvable;
+use Mvc5\Service;
 
 class Action
 {
@@ -16,11 +18,32 @@ class Action
 
     /**
      * @param $controller
+     * @return mixed
+     */
+    protected function controller($controller)
+    {
+        $controller instanceof Service && !$controller->service()
+            && $controller->service($this->service());
+
+        return $controller;
+    }
+
+    /**
+     * @param $controller
+     * @return callable|null|object
+     */
+    protected function resolve($controller)
+    {
+        return is_string($controller) ? $this->plugin($controller) : $controller;
+    }
+
+    /**
+     * @param $controller
      * @param array $args
      * @return mixed
      */
-    function __invoke($controller, array $args = [])
+    function __invoke($controller = null, array $args = [])
     {
-        return $this->call($controller, $args);
+        return $controller ? $this->call($this->controller($this->resolve($controller)), $args) : null;
     }
 }
