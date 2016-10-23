07cb8120d6480a836d5d7d201b2610398bb13d52
devosc <devosc@gmail.com>
2016-10-23 01:44:29 -0500

    error log

---
+++ src/Log/Error.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Log;
+
+use Mvc5\Arg;
+use Mvc5\Signal;
+use Exception;
+
+class Error
+{
+    /**
+     *
+     */
+    use Signal;
+
+    /**
+     * @var null
+     */
+    protected $options = [];
+
+    /**
+     * @var bool|false
+     */
+    protected $throw_exceptions = false;
+
+    /**
+     * @param bool|false $throw_exceptions
+     * @param array $options
+     */
+    function __construct($throw_exceptions = false, array $options = [])
+    {
+        $throw_exceptions && $this->throw_exceptions = $throw_exceptions;
+        $options && $this->options = $options;
+    }
+
+    /**
+     * @param $message
+     * @return bool
+     * @throws Exception
+     */
+    function __invoke($message)
+    {
+        if ($this->throw_exceptions && $message instanceof Exception) {
+            throw $message;
+        }
+
+        return $this->signal('error_log', [Arg::MESSAGE => (string) $message] + $this->options);
+    }
+}
