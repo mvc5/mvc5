ccbd326469d3f319c5a8db960e36277ef083c668
devosc <devosc@gmail.com>
2016-10-24 22:19:20 -0500

    use event for log function

---
+++ src/Log/Logger.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Log;
+
+use Mvc5\Arg;
+use Mvc5\Event\Event;
+use Mvc5\Event\Signal;
+
+class Logger
+    implements Event
+{
+    /**
+     *
+     */
+    use Signal;
+
+    /**
+     *
+     */
+    const EVENT = 'log';
+
+    /**
+     * @var bool|false
+     */
+    protected $throw_exception = false;
+
+    /**
+     * @var mixed|\Exception
+     */
+    protected $message;
+
+    /**
+     * @param bool|false $throw_exception
+     */
+    function __construct($throw_exception = false)
+    {
+        $this->throw_exception = $throw_exception;
+    }
+
+    /**
+     * @return array
+     */
+    protected function args()
+    {
+        return array_filter([
+            Arg::EVENT           => $this,
+            Arg::MESSAGE         => $this->message,
+            Arg::THROW_EXCEPTION => $this->throw_exception
+        ]);
+    }
+
+    /**
+     * @param callable $callable
+     * @param array $args
+     * @param callable $callback
+     * @return mixed
+     */
+    function __invoke(callable $callable, array $args = [], callable $callback = null)
+    {
+        $result = $this->signal($callable, $this->args() + $args, $callback);
+
+        $result && !is_bool($result) &&
+            $this->message = $result;
+
+        return $result;
+    }
+}
