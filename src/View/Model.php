d35fbf25651dd67820cf4701d6c7b4939108ff64
devosc <devosc@gmail.com>
2016-10-31 07:05:57 -0500

    new instances are not stored, remove set method, change visibility of methods to protected.

--- src/View/Model.php
+++ src/View/Model.php
@@ -16,33 +16,23 @@ trait Model
     protected $model;
 
     /**
-     * @param ViewModel $model
-     * @return void
-     */
-    function setModel(ViewModel $model)
-    {
-        $this->model = $model;
-    }
-
-    /**
      * @param array $vars
      * @param null|string $template
      * @return ViewModel
      */
-    function model(array $vars = [], $template = null)
+    protected function model(array $vars = [], $template = null)
     {
-        if (!$this->model) {
-            $this->model = (defined('static::VIEW_MODEL') && $model = constant('static::VIEW_MODEL'))
-                ? new $model : new Mvc5Model;
+        $model = $this->model ?: (
+            (defined('static::VIEW_MODEL') && $model = constant('static::VIEW_MODEL')) ? new $model : new Mvc5Model
+        );
 
-            !$template && $template = defined('static::TEMPLATE_NAME') ? constant('static::TEMPLATE_NAME') : null;
-        }
+        !$template && $template = defined('static::TEMPLATE_NAME') ? constant('static::TEMPLATE_NAME') : null;
 
-        $template && $this->model->template($template);
+        $template && $model->template($template);
 
-        $vars && $this->model->vars($vars);
+        $vars && $model->vars($vars);
 
-        return $this->model;
+        return $model;
     }
 
     /**
@@ -50,7 +40,7 @@ trait Model
      * @param array $vars
      * @return ViewModel
      */
-    function view($template = null, array $vars = [])
+    protected function view($template = null, array $vars = [])
     {
         return $this->model($vars, $template);
     }
