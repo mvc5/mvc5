1a6542130fc274a174a6a332071928692725f6a1
devosc <devosc@gmail.com>
2017-03-25 08:53:10 -0500

    tidy up

--- src/Response/Json.php
+++ src/Response/Json.php
@@ -9,21 +9,19 @@ class Json
     extends Config
 {
     /**
+     * JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_SLASHES
+     */
+    const ENCODE_OPTIONS = 79;
+
+    /**
      * @param $data
      * @param int $status
      * @param array $headers
      */
     function __construct($data, $status = 200, array $headers = [])
     {
-        parent::__construct($this->encode($data), $status, $headers + ['Content-Type' => 'application/json']);
-    }
-
-    /**
-     * @param $data
-     * @return string
-     */
-    protected function encode($data)
-    {
-        return json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_SLASHES);
+        parent::__construct(
+            json_encode($data, static::ENCODE_OPTIONS), $status, $headers + ['Content-Type' => 'application/json']
+        );
     }
 }
