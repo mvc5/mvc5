b12ee5ea9c6f9fc2ca2e852c80aedb4df8a5f9e8
devosc <devosc@gmail.com>
2017-05-03 12:07:03 -0500

    rename Plugins\ViewModel to View

--- src/Plugins/View.php
+++ src/Plugins/View.php
@@ -9,7 +9,7 @@ use Mvc5\Arg;
 use Mvc5\Template\TemplateModel;
 use Mvc5\Template\TemplateLayout;
 
-trait ViewModel
+trait View
 {
     /**
      * @param array $vars
@@ -19,22 +19,25 @@ trait ViewModel
      */
     protected function layout(array $vars = [], $template = null, $model = Arg::LAYOUT)
     {
-        return $this->plugin($model, array_filter([Arg::TEMPLATE => $template, Arg::VARS => $vars]));
+        return $this->model($vars, $template, $model);
     }
 
     /**
      * @param array $vars
      * @param null|string $template
+     * @param $model
      * @return mixed|TemplateModel|TemplateLayout
      */
-    protected function model(array $vars = [], $template = null)
+    protected function model(array $vars = [], $template = null, $model = null)
     {
         !$template && defined('static::TEMPLATE_NAME')
             && $template = constant('static::TEMPLATE_NAME');
 
-        $model = defined('static::VIEW_MODEL') ? constant('static::VIEW_MODEL') : Arg::VIEW_MODEL;
+        !$model && $model = defined('static::VIEW_MODEL') ? constant('static::VIEW_MODEL') : Arg::VIEW_MODEL;
 
-        return $this->plugin($model, [$template, $vars]);
+        $template && $vars[Arg::TEMPLATE_MODEL] = $template;
+
+        return !$vars ? $this->plugin($model) : $this->plugin($model)->with($vars);
     }
 
     /**
