f139172f62f043451bdfd34b24d0e72aded78923
devosc <devosc@gmail.com>
2017-05-10 18:02:03 -0500

    tidy up (readonly)

--- src/Config/ReadOnly.php
+++ src/Config/ReadOnly.php
@@ -28,11 +28,29 @@ trait ReadOnly
 
     /**
      * @param mixed $name
+     * @throws \Exception
+     */
+    function offsetUnset($name)
+    {
+        throw new \Exception('Invalid operation: object cannot be modified');
+    }
+
+    /**
+     * @param mixed $name
      * @param mixed $value
      * @return mixed
      * @throws \Exception
      */
     function __set($name, $value)
+    {
+        throw new \Exception('Invalid operation: object cannot be modified');
+    }
+
+    /**
+     * @param mixed $name
+     * @throws \Exception
+     */
+    function __unset($name)
     {
         throw new \Exception('Invalid operation: object cannot be modified');
     }
