3fc48889930b4f69be1c17f67c24dd5bec9d4f94
devosc <devosc@gmail.com>
2017-08-19 11:48:20 -0500

    static helper methods

--- src/Service/Context.php
+++ src/Service/Context.php
@@ -35,6 +35,37 @@ final class Context
     }
 
     /**
+     * @param array|callable|object|string $name
+     * @param array $args
+     * @param callable $callback
+     * @return callable|mixed|null|object
+     */
+    static function call($name, array $args = [], callable $callback = null)
+    {
+        return static::service()->call($name, $args, $callback);
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    static function param($name)
+    {
+        return static::service()->param($name);
+    }
+
+    /**
+     * @param string $name
+     * @param array $args
+     * @param callable $callback
+     * @return callable|mixed|null|object
+     */
+    static function plugin($name, array $args = [], callable $callback = null)
+    {
+        return static::service()->plugin($name, $args, $callback);
+    }
+
+    /**
      * @return callable|Manager|Service
      */
     static function service()
@@ -49,7 +80,7 @@ final class Context
      */
     static function __callStatic($name, array $args)
     {
-        return static::service()->call($name, $args);
+        return static::call($name, $args);
     }
 
     /**
