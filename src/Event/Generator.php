043ee3c64c9635da2bfc257b5dbf110b752237c3
devosc <devosc@gmail.com>
2017-11-28 07:26:41 -0600

    remove array reference from event generator

--- src/Event/Generator.php
+++ src/Event/Generator.php
@@ -35,22 +35,30 @@ trait Generator
      */
     protected function generate($event, array $args = [], callable $callback = null)
     {
-        return $this->traverse($event, $this->queue($event, $args), $args, $callback);
+        return $this->iterate($event, $this->start($this->queue($event, $args)), $args, $callback);
+    }
+
+    /**
+     * @param array|\Iterator $queue
+     * @return mixed
+     */
+    protected function item($queue)
+    {
+        return $queue instanceof \Iterator ? $queue->current() : current($queue);
     }
 
     /**
      * @param array|Event|\Iterator|object|string $event
      * @param array|\Iterator $queue
      * @param array $args
-     * @param callable $callback
+     * @param callable|null $callback
      * @param mixed $result
-     * @param callable $current
      * @return mixed
      */
-    protected function iterate($event, $queue, array $args, callable $callback, $result, callable $current)
+    protected function iterate($event, $queue, array $args, callable $callback = null, $result = null)
     {
         return $this->stopped($event, $queue) ? $result : $this->loop(
-            $event, $queue, $args, $callback, $this->result($event, $current($queue), $args, $callback, $result)
+            $event, $queue, $args, $callback, $this->result($event, $this->item($queue), $args, $callback)
         );
     }
 
@@ -58,20 +66,13 @@ trait Generator
      * @param array|Event|\Iterator|object|string $event
      * @param array|\Iterator $queue
      * @param array $args
-     * @param callable $callback
+     * @param callable|null $callback
      * @param mixed $result
      * @return mixed
      */
-    protected function loop($event, &$queue, array $args, callable $callback, $result)
+    protected function loop($event, $queue, array $args, callable $callback = null, $result = null)
     {
-        return $this->iterate($event, $queue, $args, $callback, $result, function(&$queue) {
-            if ($queue instanceof \Iterator) {
-                $queue->next();
-                return $queue->current();
-            }
-
-            return next($queue);
-        });
+        return $this->iterate($event, $this->step($queue), $args, $callback, $result);
     }
 
     /**
@@ -86,15 +87,14 @@ trait Generator
 
     /**
      * @param array|Event|\Iterator|object|string $event
-     * @param mixed $listener
+     * @param mixed $current
      * @param array $args
      * @param callable|null $callback
-     * @param mixed $result
      * @return mixed
      */
-    protected function result($event, $listener, array $args = [], callable $callback = null, $result = null)
+    protected function result($event, $current, array $args = [], callable $callback = null)
     {
-        return !$listener ? $result : $this->emit($event, $this->callable($listener), $args, $callback);
+        return $this->emit($event, $this->callable($current), $args, $callback);
     }
 
     /**
@@ -119,6 +119,16 @@ trait Generator
     }
 
     /**
+     * @param array|\Iterator $queue
+     * @return array|\Iterator
+     */
+    protected function step($queue)
+    {
+        $queue instanceof \Iterator ? $queue->next() : next($queue);
+        return $queue;
+    }
+
+    /**
      * @param Event|mixed $event
      * @param array|\Iterator $queue
      * @return bool
@@ -127,19 +137,5 @@ trait Generator
     {
         return ($event instanceof Event && $event->stopped()) ||
             ($queue instanceof \Iterator ? !$queue->valid() : null === key($queue));
-    }
-
-    /**
-     * @param array|Event|\Iterator|object|string $event
-     * @param array|\Iterator $queue
-     * @param array $args
-     * @param callable $callback
-     * @return mixed
-     */
-    protected function traverse($event, $queue, array $args, callable $callback)
-    {
-        return $this->iterate($event, $this->start($queue), $args, $callback, null, function($queue) {
-            return $queue instanceof \Iterator ? $queue->current() : current($queue);
-        });
     }
 }
