e0d64eabfd342259e57ac5c867fdde47b86c32e4
devosc <devosc@gmail.com>
2018-04-03 21:01:36 -0500

    http response return type

--- src/Plugins/Response.php
+++ src/Plugins/Response.php
@@ -6,6 +6,7 @@
 namespace Mvc5\Plugins;
 
 use Mvc5\Arg;
+use Mvc5\Http;
 
 trait Response
 {
@@ -13,9 +14,9 @@ trait Response
      * @param $data
      * @param int $status
      * @param array $headers
-     * @return \Mvc5\Response\JsonResponse|mixed
+     * @return Http\Response|\Mvc5\Response\JsonResponse|mixed
      */
-    protected function json($data, int $status = 200, array $headers = [])
+    protected function json($data, int $status = 200, array $headers = []) : Http\Response
     {
         return $this->plugin(Arg::RESPONSE_JSON, [$data, $status, $headers]);
     }
@@ -25,9 +26,9 @@ trait Response
      * @param int $status
      * @param array $headers
      * @param array $config
-     * @return \Mvc5\Response\RedirectResponse|mixed
+     * @return Http\Response|\Mvc5\Response\RedirectResponse|mixed
      */
-    protected function redirect($url, int $status = 302, array $headers = [], array $config = [])
+    protected function redirect($url, int $status = 302, array $headers = [], array $config = []) : Http\Response
     {
         return $this->plugin(Arg::RESPONSE_REDIRECT, [$url, $status, $headers, $config]);
     }
@@ -37,9 +38,9 @@ trait Response
      * @param int|null $status
      * @param array $headers
      * @param array $config
-     * @@return \Mvc5\Response\Response|mixed
+     * @return Http\Response|\Mvc5\Response\Response|mixed
      */
-    protected function response($body = null, int $status = null, $headers = [], array $config = [])
+    protected function response($body = null, int $status = null, $headers = [], array $config = []) : Http\Response
     {
         return $this->plugin(Arg::RESPONSE, [$body, $status, $headers, $config]);
     }
