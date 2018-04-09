9c5258ca3d7141a8e014e8a226d3984776f9403f
devosc <devosc@gmail.com>
2018-04-08 22:57:12 -0500

    template model return type

--- src/Plugins/View.php
+++ src/Plugins/View.php
@@ -6,6 +6,8 @@
 namespace Mvc5\Plugins;
 
 use Mvc5\Arg;
+use Mvc5\Template\TemplateLayout;
+use Mvc5\Template\TemplateModel;
 
 trait View
 {
@@ -13,9 +15,9 @@ trait View
      * @param array $vars
      * @param string|null $template
      * @param string $model
-     * @return \Mvc5\Template\TemplateLayout|mixed
+     * @return TemplateLayout|mixed
      */
-    protected function layout(array $vars = [], string $template = null, string $model = Arg::LAYOUT)
+    protected function layout(array $vars = [], string $template = null, string $model = Arg::LAYOUT) : TemplateLayout
     {
         return $this->model($vars, $template, $model);
     }
@@ -24,9 +26,9 @@ trait View
      * @param array $vars
      * @param string|null $template
      * @param string|null $model
-     * @return \Mvc5\Template\TemplateModel|\Mvc5\Template\TemplateLayout|mixed
+     * @return TemplateModel|mixed
      */
-    protected function model(array $vars = [], string $template = null, string $model = null)
+    protected function model(array $vars = [], string $template = null, string $model = null) : TemplateModel
     {
         !$model && $model = defined('static::VIEW_MODEL') ? constant('static::VIEW_MODEL') : Arg::VIEW_MODEL;
 
@@ -48,9 +50,9 @@ trait View
     /**
      * @param string|null $template
      * @param array $vars
-     * @return \Mvc5\Template\TemplateModel|mixed
+     * @return TemplateModel|mixed
      */
-    protected function view(string $template = null, array $vars = [])
+    protected function view(string $template = null, array $vars = []) : TemplateModel
     {
         return $this->model($vars, $template);
     }
