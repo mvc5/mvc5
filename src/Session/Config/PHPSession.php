2b3b2ed74545a80b21b887e6ff6813ba3ac34728
devosc <devosc@gmail.com>
2018-12-21 18:22:41 -0600

    tidy up

--- src/Session/Config/PHPSession.php
+++ src/Session/Config/PHPSession.php
@@ -72,8 +72,9 @@ trait PHPSession
      */
     function destroy(bool $remove_session_cookie = true) : bool
     {
-        $remove_session_cookie &&
-            PHPCookies::send([Arg::NAME => $this->name(), Arg::VALUE => '', Arg::EXPIRE => 946706400] + session_get_cookie_params());
+        $remove_session_cookie && PHPCookies::send(
+            [Arg::NAME => $this->name(), Arg::VALUE => '', Arg::EXPIRE => 946706400] + session_get_cookie_params()
+        );
 
         return session_destroy();
     }
