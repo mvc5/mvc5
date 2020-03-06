c3b00d3996fb747fb49f69f1e01b44126a56d689
devosc <devosc@gmail.com>
2020-03-05 19:47:37 -0600

    scope plugin always creates a new app

--- src/Plugin/Scope.php
+++ src/Plugin/Scope.php
@@ -5,23 +5,25 @@
 
 namespace Mvc5\Plugin;
 
+use Mvc5\Arg;
+use Mvc5\App;
 use Mvc5\Service\Manager;
 use Mvc5\Service\Service;
 
 /**
- * Creates an object with a Plugins container and sets the object as the scope of the anonymous functions. If the
- * object is cloned, it will need a clone method to workaround the recursion problem.
+ * Creates a model with a scopable plugins container and sets the model as the scope of the anonymous functions.
  */
 class Scope
     extends Call
 {
     /**
-     * @param Manager $context
+     * @param array $plugins
      * @param string $name
      */
-    function __construct(Manager $context, string $name)
+    function __construct(array $plugins, string $name)
     {
-        parent::__construct([$this, 'scope'], [$context, new Link, $name]);
+        parent::__construct([$this, 'scope'], [
+            new Plugin(App::class, [[Arg::SERVICES => $plugins], null, true, true]), new Link, $name]);
     }
 
     /**
